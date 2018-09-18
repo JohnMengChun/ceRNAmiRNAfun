e7f80bf3bee84562fdbebb6d0aa0f39658dc2808
JohnMengChun <40553123+JohnMengChun@users.noreply.github.com>
2018-09-18 23:50:59 +0900

    Add files via upload

---
+++ R/data_miRNA_total.R
@@ -0,0 +1,11 @@
+#' miRNA list of \code{\link{dictionary}} about lung cancer
+#'
+#' This miRNA dataset is originally from GSE19536. It contains
+#' 256 miRNA names
+#'
+#' @format A vector with 256miRNA names
+#'
+#' @return vector
+#' @source NCBI GEO:
+#'  \url{http://www.ncbi.nlm.nih.gov/geo/}
+"miRNA_total"
