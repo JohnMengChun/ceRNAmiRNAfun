582629fdec2cd7f8f93c384d9ed5ff89e37f9a68
JohnMengChun <40553123+JohnMengChun@users.noreply.github.com>
2019-04-30 16:38:12 +0800

    Add files via upload

--- R/data_miRhubgeneoutput.R
+++ R/data_miRhubgeneoutput.R
@@ -1,13 +1,17 @@
-#' A dataframe with information of miRNAs and corresponding bridging ceRNA triplets, Genes, and ceRNA triplets
+#' a dataframe formats with miRNA name 
+#' with the number of bridging ceRNA triplets 
+#' and the corresponding genes with the number 
+#' of ceRNA triplets.
 #'
-#' This dataframe is generated from function miRhubgene.
+#' This dataframe is generated from \link{miRhubgene},
 #' 
 #'
-#' @format A a dataframe with miRNA's name in 
-#' the first column, bridging ceRNA triplets, Genes
-#' and ceRNA triplets as order in the following columns.
+#' @format A dataframe with miRNA name in the 
+#' first column, the number of bridging ceRNA triplets, 
+#' corresponding gene and the number of ceRNA triplets
+#' in the following columns
 #' 
-#'  
+#' 
 #' @return dataframe
 #' @source TCGA data portal:
 #'  \url{https://tcga-data.nci.nih.gov/docs/publications/tcga/}
